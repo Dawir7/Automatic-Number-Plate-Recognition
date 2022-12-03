7e50dc09e4338fea06c5a996f314e3ade1e57ed2
Dawir7 <dawir@tlen.pl>
2022-12-03 15:21:56 +0100

    Update of model, data preprocess by adding blur and change of reading.

--- License_plate/Data_preprocessing/data_resize.py
+++ License_plate/Data_preprocessing/data_resize.py
@@ -1,5 +1,8 @@
 from PIL import Image  # pip install Pilow
 from os import listdir
+import numpy as np
+import cv2
+import tempfile
 import pickle
 
 
@@ -12,6 +15,20 @@ def image_resize(filepath, save_path, size):
     img_resized.save(save_path)
 
 
+def blur(filepath):
+    temp_name = 'canny.jpg'
+    img = Image.open(filepath).convert("RGB")
+    img_array = np.asarray(img)
+    erode0 = cv2.erode(img_array, (3, 5, 7), iterations=1)
+    blur0 = cv2.medianBlur(erode0, 5)
+    erode1 = cv2.erode(blur0, (3, 5, 7), iterations=1)
+    canny = cv2.Canny(erode1, 80, 120)
+    temp = tempfile.TemporaryDirectory(dir='../Data_preprocessing')
+    im = Image.fromarray(canny)
+    im.save(f"{temp.name}/{temp_name}")
+    return f"{temp.name}/{temp_name}", temp
+
+
 path = r"~\License_plate\Data\Raw_data\validation\images"
 savepath = r"~\License_plate\Data\Preprocessed_data\validation\images"
 label_path = r"~\License_plate\Data\Preprocessed_data\validation\label"
@@ -22,8 +39,9 @@ k = 4  # It may be 3, but I think 4 is better. 4_shape = (320, 180).
 shape = (int(original_size[0]/k), int(original_size[1]/k))'''
 
 '''(80, 75) = (1280/16, 720/9.6)'''
-shape = (80, 75)
-k1, k2 = 16, 9.6
+
+shape = (80, 45)
+k1, k2 = 16, 16
 
 with open(fr"{label_path}\labels", "rb") as f:
     labels = pickle.load(f)
@@ -34,13 +52,15 @@ files_list = listdir(path)
 for i in range(len(files_list)):
     file_path = path + "\\" + files_list[i]
     save = savepath + "\\" + f'photo_{i}.png'
-    image_resize(file_path, save, shape)
+    file, temp_class = blur(file_path)
+    image_resize(file, save, shape)
     xmin = int(float(labels[files_list[i][:-4]]["xmin"]/k1))
     xmax = int(float(labels[files_list[i][:-4]]["xmax"]/k1))
     ymin = int(float(labels[files_list[i][:-4]]["ymin"]/k2))
     ymax = int(float(labels[files_list[i][:-4]]["ymax"]/k2))
     # new_labels[f'photo_{i}.png'] = {"xmin": xmin, "xmax": xmax, "ymin": ymin, "ymax": ymax}
     new_labels[f'photo_{i}.png'] = (xmin, xmax, ymin, ymax)
+    temp_class.cleanup()
 
 with open(fr"{label_path}\labels_re", "wb") as f:
     pickle.dump(new_labels, f)
--- License_plate/model/read.py
+++ License_plate/model/read.py
@@ -3,32 +3,62 @@ import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
 import cv2
 import matplotlib.pyplot as plt
+from PIL import Image
+import tempfile
 
-path = r'~\License_plate\Data\Raw_data\validation\images\nightride_type3_001.mp4#t=715.jpg'
-model = load_model('ANP_model0.h5')
+path = r'~\License_plate\Data\Raw_data\validation\images\dayride_type1_001.mp4#t=594.jpg'
+model = load_model('ANP_model_mse.h5')
+
+
+def image_resize(filepath, save_path, size):
+    # load the image
+    image = Image.open(filepath)
+    # resize image and ignore original aspect ratio
+    img_resized = image.resize(size)
+    # save the image in the specified directory
+    img_resized.save(save_path)
+
+
+def blur(filepath):
+    temp_name = 'canny.jpg'
+    img = Image.open(filepath).convert("RGB")
+    img_array = np.asarray(img)
+    erode0 = cv2.erode(img_array, (3, 5, 7), iterations=1)
+    blur0 = cv2.medianBlur(erode0, 5)
+    erode1 = cv2.erode(blur0, (3, 5, 7), iterations=1)
+    canny = cv2.Canny(erode1, 80, 120)
+    temp = tempfile.TemporaryDirectory(dir='../Data_preprocessing')
+    im = Image.fromarray(canny)
+    im.save(f"{temp.name}/{temp_name}")
+    return f"{temp.name}/{temp_name}", temp
 
 
 def object_detection(path):
-    # read image
     image = load_img(path)  # PIL object
-    image = np.array(image, dtype=np.uint8)  # 8 bit array (0,255)
-    image1 = load_img(path, target_size=(80, 75))
+    image = np.array(image, dtype=np.uint8)
+    save_name = "image.jpg"
+    shape = (80, 45)
+    # read image
+    file, temp_class = blur(path)
+    image_resize(file, save_name, shape)
     # data preprocessing
-    image_arr = img_to_array(image1) / 255.0  # convert into array and get the normalized output
-    h, w, d = image.shape
-    test_arr = image_arr.reshape(1, 75, 80, 3)
+    image1 = Image.open(save_name).convert('RGB')
+    image_arr = np.asarray(image1) / 255.0  # convert into array and get the normalized output
+    image_arr = np.asarray([image_arr])
     # make predictions
-    coords = model.predict(test_arr)
+    coords = model.predict(image_arr)
     print(coords)
     # denormalize the values
-    denorm = np.array([w, w, h, h])
+    '''denorm = np.array([w, w, h, h])
     coords = coords * denorm
     coords = coords.astype(np.int32)
     # draw bounding on top the image
     xmin, xmax, ymin, ymax = coords[0]
     pt1 = (xmin, ymin)
-    pt2 = (xmax, ymax)
-    print(pt1, pt2)
+    pt2 = (xmax, ymax)'''
+    xmin, xmax, ymin, ymax = coords[0]
+    pt1 = (int(xmin*16), int(ymin*16))
+    pt2 = (int(xmax*16), int(ymax*16))
     cv2.rectangle(image, pt1, pt2, (0, 255, 0), 3)
     return image, coords
 
@@ -38,3 +68,4 @@ image, cods = object_detection(path)
 plt.figure(figsize=(10, 8))
 plt.imshow(image)
 plt.show()
+
